600232c9a0d0fa321de2da3a828856a924494614
Deb-bie <asamoahjaeldeborah@gmail.com>
2026-05-06 23:45:06 -0500

    feature: loan status enum

---
+++ springboot-service/src/main/java/com/example/underwriting/enums/LoanStatus.java
@@ -0,0 +1,12 @@
+package com.example.underwriting.enums;
+
+public enum LoanStatus {
+    SUBMITTED, 
+    DOCUMENT_REVIEW, 
+    IN_UNDERWRITING, 
+    CONDITIONALLY_APPROVED,
+    CONDITIONS_CLEARED, 
+    APPROVED, 
+    DECLINED, 
+    WITHDRAWN
+}
